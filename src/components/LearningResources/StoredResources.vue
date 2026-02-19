962fc62a6d3cd629bb79026ad3ec77f8513e7010
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-19 13:48:18 -0500

    added learning resources components

---
+++ src/components/LearningResources/StoredResources.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <LearningResource
+      v-for="resource in resources"
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    />
+  </ul>
+</template>
+
+<script>
+import LearningResource from "./LearningResource.vue";
+
+export default {
+  components: {
+    LearningResource,
+  },
+  props: {
+    resources: Array,
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>

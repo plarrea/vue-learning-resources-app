584309e9cac74f42961289556246aaae81a0d15a
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-19 13:48:47 -0500

    remove unused components and updated app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <StoredResources :resources="storedResources" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StoredResources from "./components/LearningResources/StoredResources.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    StoredResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official Vue.js documentation.",
+          link: "https://vuejs.org",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learn to google...",
+          link: "https://google.org",
+        },
+      ],
+    };
+  },
+  methods: {},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
 }
 </style>

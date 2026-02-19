a43354527a02750b8c211bbbe8d21491b23642b1
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-19 13:50:47 -0500

    added app header component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
+  <TheHeader title="Learning Resources" />
   <StoredResources :resources="storedResources" />
 </template>
 
 <script>
+import TheHeader from "./components/Layouts/TheHeader.vue";
 import StoredResources from "./components/LearningResources/StoredResources.vue";
 
 export default {
   components: {
+    TheHeader,
     StoredResources,
   },
   data() {
